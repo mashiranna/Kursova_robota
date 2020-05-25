62ef9dcb81062d21fb75d4a091ee41ee93a663f7
mashiranna <55777589+mashiranna@users.noreply.github.com>
2020-05-25 16:20:29 +0300

    Add files via upload

---
+++ Huffman1.3/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Huffman1._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Beige;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(17, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(955, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(146, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Закодувати";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Beige;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 399);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(450, 200);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(663, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Декодувати";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.Beige;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(522, 399);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(450, 200);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.Beige;
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox3.Location = new System.Drawing.Point(17, 104);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(955, 219);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(417, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Початковий текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(435, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Аналіз тексту";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(214, 372);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Код";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(674, 372);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Декодованый текст";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(984, 611);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Huffman coding";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Huffman1.3/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Huffman1._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Node
+        {
+            public char Symbol { get; set; }
+            public int Frequency { get; set; }
+            public Node Right { get; set; }
+            public Node Left { get; set; }
+
+            public List<bool> Traverse(char symbol, List<bool> data)
+            {
+                // Leaf
+                if (Right == null && Left == null)
+                {
+                    if (symbol.Equals(this.Symbol))
+                        return data;
+                    else
+                        return null;
+                }
+                else
+                {
+                    List<bool> left = null;
+                    List<bool> right = null;
+
+                    if (Left != null)
+                    {
+                        List<bool> leftPath = new List<bool>();
+                        leftPath.AddRange(data);
+                        leftPath.Add(false);
+
+                        left = Left.Traverse(symbol, leftPath);
+                    }
+
+                    if (Right != null)
+                    {
+                        List<bool> rightPath = new List<bool>();
+                        rightPath.AddRange(data);
+                        rightPath.Add(true);
+                        right = Right.Traverse(symbol, rightPath);
+                    }
+
+                    if (left != null)
+                        return left;
+                    else
+                        return right;
+                }
+            }
+            }
+        public class HuffmanTree
+        {
+            private List<Node> nodes = new List<Node>();
+            public Node Root { get; set; }
+            public Dictionary<char, int> Frequencies = new Dictionary<char, int>();
+
+            public void Build(string source)
+            {
+                for (int i = 0; i < source.Length; i++)
+                {
+                    if (!Frequencies.ContainsKey(source[i]))
+                    {
+                        Frequencies.Add(source[i], 0);
+                    }
+
+                    Frequencies[source[i]]++;
+                }
+
+                foreach (KeyValuePair<char, int> symbol in Frequencies)
+                {
+                    nodes.Add(new Node() { Symbol = symbol.Key, Frequency = symbol.Value });
+                }
+
+                while (nodes.Count > 1)
+                {
+                    List<Node> orderedNodes = nodes.OrderBy(node => node.Frequency).ToList<Node>();
+
+                    if (orderedNodes.Count >= 2)
+                    {
+                        // Take first two items
+                        List<Node> taken = orderedNodes.Take(2).ToList<Node>();
+
+                        // Create a parent node by combining the frequencies
+                        Node parent = new Node()
+                        {
+                            Symbol = '*',
+                            Frequency = taken[0].Frequency + taken[1].Frequency,
+                            Left = taken[0],
+                            Right = taken[1]
+                        };
+
+                        nodes.Remove(taken[0]);
+                        nodes.Remove(taken[1]);
+                        nodes.Add(parent);
+                    }
+                    this.Root = nodes.FirstOrDefault();
+                }
+            }
+
+            public List<string> code = new List<string>();
+            public IEnumerable<string> codec;
+
+            public BitArray Encode(string source)
+            {
+                List<bool> encodedSource = new List<bool>();
+
+                for (int i = 0; i < source.Length; i++)
+                {
+                    List<bool> encodedSymbol = this.Root.Traverse(source[i], new List<bool>());
+                    encodedSource.AddRange(encodedSymbol);
+                    this.code.Add("");
+
+                    foreach (bool a in encodedSymbol.ToArray())
+                        this.code[i] += Convert.ToInt32(a).ToString();
+                }
+                // IEnumerable<string> 
+                codec = code.Distinct();
+                BitArray bits = new BitArray(encodedSource.ToArray());
+                return bits;
+            }
+
+            public string Decode(BitArray bits)
+            {
+                Node current = this.Root;
+                string decoded = "";
+
+                foreach (bool bit in bits)
+                {
+                    if (bit)
+                    {
+                        if (current.Right != null)
+                            current = current.Right;
+                    }
+                    else
+                    {
+                        if (current.Left != null)
+                            current = current.Left;
+                    }
+                    if (IsLeaf(current))
+                    {
+                        decoded += current.Symbol;
+                        current = this.Root;
+                    }
+                }
+                return decoded;
+            }   
+
+            public bool IsLeaf(Node node)
+            {
+                return (node.Left == null && node.Right == null);
+            }
+
+        }
+        public HuffmanTree htree;
+        public BitArray encoded;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            richTextBox1.Text = "";
+            htree.Build(textBox1.Text);
+            encoded = htree.Encode(textBox1.Text);
+            foreach (bool bit in encoded)
+            {
+                richTextBox1.Text += ((bit ? 1 : 0).ToString() + " ");
+            }
+            foreach (KeyValuePair<char, int> item in htree.Frequencies)
+            {
+                richTextBox3.Text += ("Символ: " + item.Key + "\t Кількість: " + item.Value.ToString() + "\t Частота: " + (Math.Round((float)item.Value / textBox1.Text.Length, 3)) + "\t Код " + htree.codec.ToArray()[i] + "\n");
+                i++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "";
+            richTextBox2.AppendText(htree.Decode(encoded));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            htree = new HuffmanTree();
+            textBox1.Width = this.Size.Width;
+            richTextBox1.Width = this.Size.Width;
+            richTextBox2.Width = this.Size.Width;
+            richTextBox3.Width = this.Size.Width;
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
